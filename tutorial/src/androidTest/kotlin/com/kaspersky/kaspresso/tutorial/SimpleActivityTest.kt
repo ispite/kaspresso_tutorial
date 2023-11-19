3f3a1e088bf87ac4a1e6b8be8c929006d823762c
Denis Solovyov <infinityx93@gmail.com>
2023-11-19 21:03:10 +0300

    first end-to-end test

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/SimpleActivityTest.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/SimpleActivityTest.kt
@@ -1,25 +1,33 @@
-package com.kaspersky.kaspresso.tutorial.screen
+package com.kaspersky.kaspresso.tutorial
 
 import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import com.kaspersky.kaspresso.tutorial.simple.SimpleActivity
+import com.kaspersky.kaspresso.tutorial.screen.MainScreen
+import com.kaspersky.kaspresso.tutorial.screen.SimpleScreen
 import org.junit.Rule
 import org.junit.Test
 
 class SimpleActivityTest : TestCase() {
 
     @get:Rule
-    val activityRule = activityScenarioRule<SimpleActivity>()
+    val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
     fun test() {
+        MainScreen {
+            simpleActivityButton {
+                isVisible()
+                isClickable()
+                click()
+            }
+        }
         SimpleScreen {
             simpleTitle.isVisible()
             inputText.isVisible()
             changeTitleButton.isVisible()
-            simpleTitle.hasText("Default title")
+            simpleTitle.hasText(R.string.simple_activity_default_title)
             inputText.hasEmptyText()
-            inputText.hasHint("input text")
+            inputText.hasHint(R.string.simple_activity_input_hint)
             inputText.replaceText("New title")
             changeTitleButton.click()
             simpleTitle.hasText("New title")
