c95c0527315c8c68e202cb5bec4f9f7986632561
Denis Solovyov <infinityx93@gmail.com>
2023-11-19 22:47:44 +0300

    test with steps

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/WifiScreenTest.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/WifiScreenTest.kt
@@ -1,6 +1,7 @@
 package com.kaspersky.kaspresso.tutorial
 
 import androidx.test.ext.junit.rules.activityScenarioRule
+import com.kaspersky.kaspresso.device.exploit.Exploit
 import com.kaspersky.kaspresso.tutorial.screen.MainScreen
 import com.kaspersky.kaspresso.tutorial.screen.WifiScreen
 import org.junit.Rule
@@ -12,27 +13,45 @@ class WifiScreenTest : com.kaspersky.kaspresso.testcases.api.testcase.TestCase()
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun test() {
-        MainScreen {
-            wifiActivityButton {
-                isVisible()
-                isClickable()
-                click()
+    fun test() = before {
+        device.network.toggleWiFi(true)
+        device.exploit.setOrientation(Exploit.DeviceOrientation.Portrait)
+    }
+        .after {
+            device.network.toggleWiFi(true)
+            device.exploit.setOrientation(Exploit.DeviceOrientation.Portrait)
+        }
+        .run {
+            step("Open target screen") {
+                MainScreen {
+                    wifiActivityButton {
+                        isVisible()
+                        isClickable()
+                        click()
+                    }
+                }
+            }
+
+            WifiScreen {
+                step("Check default state") {
+                    wifiStatus.hasEmptyText()
+                }
+
+                step("Check enabled state") {
+                    checkWifiButton.click()
+                    wifiStatus.hasText(R.string.enabled_status)
+                }
+
+                step("Check disabled state") {
+                    device.network.toggleWiFi(false)
+                    checkWifiButton.click()
+                    wifiStatus.hasText(R.string.disabled_status)
+                }
+
+                step("Rotate and check again") {
+                    device.exploit.rotate()
+                    wifiStatus.hasText(R.string.disabled_status)
+                }
             }
         }
-        WifiScreen {
-            wifiStatus.hasEmptyText()
-            checkWifiButton.click()
-            wifiStatus.hasText(R.string.enabled_status)
-
-            device.network.toggleWiFi(false)
-            checkWifiButton.click()
-            wifiStatus.hasText(R.string.disabled_status)
-
-            device.exploit.rotate()
-            wifiStatus.hasText(R.string.disabled_status)
-            // My addition
-            device.network.toggleWiFi(true)
-        }
-    }
 }
